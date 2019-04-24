166ba625606e9c7cd053c409b412e43344ced315
Patrick Andersen <44160946+sanidk@users.noreply.github.com>
2019-04-24 10:31:38 +0200

    så deeet

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private float lul;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
